0bc746f0d8e52011578d77154e755f38c225992d
turmoil2k <unconfigured@null.spigotmc.org>
2020-03-29 07:55:14 +0400

    commiting changes

---
+++ Assets/Scenes/Studio/BT_AI/Scripts/Node.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Node : MonoBehaviour
+{
+    public abstract void ActionNode(BTContext owner); //(Context owner);
+}
